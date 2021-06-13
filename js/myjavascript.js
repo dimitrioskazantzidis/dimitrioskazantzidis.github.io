059fc56a31bcccc3d37bebb954edfa3136ed6359
kazantzidhsdimitris <kazantzidhsdimitris@gmail.com>
2021-06-13 15:39:03 +0300

    first commit

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -11,6 +11,31 @@ document.getElementById('icon').addEventListener('click',function(){
     }
 );
 
+function user(){
+	
+	var user = document.getElementById('user_info');
+	
+	if( sessionStorage.getItem('first name')!==null || sessionStorage.getItem('last name')!==null ){
+		user.innerHTML= "Logged in as temporary user " + sessionStorage.getItem('first name') +" "+ sessionStorage.getItem('last name');
+	} else {	
+		user.innerHTML= "Logged in as " + localStorage.getItem('first name') +" "+ localStorage.getItem('last name');
+	}
+}
+
+user();
+
+
+document.getElementById('logout').addEventListener('click',function(){
+	
+	sessionStorage.removeItem('first name');
+	sessionStorage.removeItem('last name');
+	localStorage.removeItem('first name');
+	localStorage.removeItem('last name');
+	window.location.href="../index.html";
+},false);
+
+
+
 
 
 
